684ac9516cab9eda621712d7f4d09c3ddac2b8e4
Binh <binhqd@gmail.com>
2014-05-02 09:51:01 +0700

    Update params.php
    Clean some content

--- jlprotected/config/development/params.php
+++ jlprotected/config/development/params.php
@@ -5,23 +5,23 @@ return array(
 	// this is displayed in the header section
 	'title' => 'Justlook',
 	// this is used in error pages
-	'adminEmail' => 'dunghd@toancauxanh.vn',
-	'systemSalt' => 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi',
+	'adminEmail' => '',
+	'systemSalt' => '',
 	'sitename' => "JustLook",
 	'mailer' => array(
-		'host' => 'smtp.gmail.com',
-		'port' => '587',
-		'secure' => 'tls',
-		'username' => 'justlook.com.au@gmail.com',
-		'password' => 'toancauxanh',
-		'name' => 'JustLook',
+		'host' => '',
+		'port' => '',
+		'secure' => '',
+		'username' => '',
+		'password' => '',
+		'name' => '',
 	),
 	'roles' => array(
-		'SADMIN' => 'Super Administrator',
-		'ADMIN' => 'Admin',
-		'BUSINESS' => 'Business',
-		'AWAITING' => 'Awaiting',
-		'MEMBER' => 'Member'
+		'SADMIN' => '',
+		'ADMIN' => '',
+		'BUSINESS' => '',
+		'AWAITING' => '',
+		'MEMBER' => ''
 	),
 	'formats' => array(
 		'date' => 'd M Y',
@@ -30,22 +30,17 @@ return array(
 	// the copyright information displayed in the footer section
 	'copyrightInfo' => 'Copyright &copy; 2012 by Green Global.',
 	// FB APP ID
-	'fbAppId' => '453345281375610',
+	'fbAppId' => '',
 	// FB APP Secret
-	'fbAppSecret' => 'd0bae04b728117e36d4511001c3782bc',
+	'fbAppSecret' => '',
 	// Justlook Page ID
-	'fbPageId' => '303761709659238',
+	'fbPageId' => '',
 	// FB admin
-	'fbAdmin' => '100003109063760',
-// 	'serverSphinx' => $info['local']['development']['sphinx']['server'],
-// 	'connectSphinx' => "mysql:host={$info['local']['development']['sphinx']['server']};port={$info['local']['development']['sphinx']['sqlport']}",
-// 	'notificationServer'	=> 'http://49.156.53.64:7475'
-	'notificationServer' => 'http://192.168.1.109',
-//	'notificationServer'	=> 'http://dev.look.vn:8080'
+	'fbAdmin' => '',
 	'gearmanPort' => '5900',
 	'cacheResetTime' => 1800,
 	'mailContact' => array(
-		'mail' => 'contact@justlook.com.au'
+		'mail' => ''
 	),
 	'device' => array(
 		'type' => 'pc'
@@ -62,22 +57,22 @@ return array(
 	),
 	'OAuth'	=> array(
 		'Gmail'	=>	array(
-			'application_name' => 'MyZone',
-			'oauth2_client_id' => '728945824814.apps.googleusercontent.com',
-			'oauth2_client_secret' => 'FEa42tDv2P2GorOwTJfvpLmf',
+			'application_name' => '',
+			'oauth2_client_id' => '',
+			'oauth2_client_secret' => '',
 			'site_name' => 'myzone.localhost.com',
 			'oauth2_redirect_uri'	=> 'http://myzone.localhost.com/google/connect',
 		),
 		'Yahoo'		=> array(
-			'app_id'			=> 'M05K607i',
-			'consumer_key'		=> 'dj0yJmk9M1ZSSE80Yjg1Z0x0JmQ9WVdrOVRUQTFTell3TjJrbWNHbzlNamM1TlRnM056WXkmcz1jb25zdW1lcnNlY3JldCZ4PTA0',
-			'consumer_secret'	=> '90f1687ff4b12e48a33c177765c938adaddb5c5d',
+			'app_id'			=> '',
+			'consumer_key'		=> '',
+			'consumer_secret'	=> '',
 			'connected_path' 	=> 'Connected.php',
 			're_url'			=> "Connected.php"
 		),
 		'Facebook'	=> array(
-			'appId'  => '141173296063550',
-			'secret' => '071334240e16b3237b825ca78ae2d94b',
+			'appId'  => '',
+			'secret' => '',
 			'cookie' => true
 		)
 	),
@@ -88,9 +83,9 @@ return array(
 		'S3URL'	=> 'http://static.youlook.net',
 		'S3'	=> array(
 			'upload'	=> array(
-				'accessKey'	=> 'AKIAJVYJA77SWHNFNDJA',
-				'secretKey'	=> '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',
-				'bucket'	=> 'static.youlook.net',
+				'accessKey'	=> '',
+				'secretKey'	=> '',
+				'bucket'	=> '',
 			)
 		)
 	)
